55bd745a9febfc50bf769812103bc01d910f9b0d
gurdian0614 <ivan.gurdian@outlook.com>
2024-08-21 20:08:21 -0600

    Creando modelo Empleado

---
+++ CRUDQ32024/Models/Empleado.cs
@@ -0,0 +1,19 @@
+﻿
+using SQLite;
+
+namespace CRUDQ32024.Models
+{
+    public class Empleado
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [NotNull]
+        public string Nombre { get; set; }
+
+        [NotNull]
+        public string Email { get; set; }
+
+        public string Direccion { get; set; }
+    }
+}
